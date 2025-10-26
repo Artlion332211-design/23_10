211193ca83ed3105aa73d8ba47a731267d880079
Artur <artlion332211@gmail.com>
2025-10-26 22:40:13 +0200

    Dz2.2

--- L2/Dz2.2.py
+++ L2/Dz2.2.py
@@ -1,12 +1,16 @@
-n = int(input("Введи 5-значне число: "))
-a, b = divmod(n,10)
-#print(a) #123
-print(b) #4
-a, b = divmod(a,10)
-#print(a)
-print(b)
-a, b = divmod(a,10)
-print(b)
-a, b = divmod(a,10)
-print(b)
-print(a)
+n =int(input("Введи 5-значне число: "))
+a,b = divmod(n,10)
+z=b
+#print(b)
+#print(a,b)
+a,b = divmod(a,10)
+#print(a,b)
+z=z * 10 + b
+#print(z)
+a,b = divmod(a,10)
+z=z * 10 + b
+a,b = divmod(a,10)
+z=z * 10 + b
+z = z * 10 + a
+
+print(z)
